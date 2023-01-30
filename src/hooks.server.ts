4b134e2a32f24bccff95a41a5033dce2301e2aa9
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2023-01-30 20:53:45 +0200

    removed logging from hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,7 +5,6 @@ import { redirect, type Handle } from '@sveltejs/kit';
 export const handle = (async ({ event, resolve }) => {
   // setup user info
   const accessToken: string | undefined = event.cookies.get(NHOST_ACCESS_TOKEN);
-  console.log('setaccessToken', accessToken);
   if (accessToken) {
     // set the session information for this event for houdini
     setSession(event, { accessToken });
