88315d57b1505046cc4f0caacef2e457240649cb
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2023-01-30 22:11:26 +0200

    fixed issue with token straight after auth

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,19 @@
 import { browser } from '$app/environment';
 import { PUBLIC_GRAPHQL_URL } from '$env/static/public';
 import { HoudiniClient, type RequestHandler } from '$houdini';
+import { NHOST_ACCESS_TOKEN } from '$lib/nhost';
+import Cookies from 'js-cookie';
 
 const requestHandler: RequestHandler = async ({ fetch, text = '', variables = {}, session }) => {
   const url = PUBLIC_GRAPHQL_URL;
-  const token = session?.accessToken as string | null | undefined;
+  let token = session?.accessToken as string | null | undefined;
   if (!token) {
-    console.error('No token found in session. In browser:', browser);
+    if (browser) {
+      token = Cookies.get(NHOST_ACCESS_TOKEN);
+    }
+    if (!token) {
+      console.error('No token found in session. In browser:', browser);
+    }
   }
   const result = await fetch(url, {
     method: 'POST',
